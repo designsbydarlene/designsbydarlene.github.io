facb4f85efdfd321da1094bebe179e59acdc632d
designsbydarlene <agbayani.darlene@gmail.com>
2024-05-03 23:32:31 -0400

    add content to website individual pages

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('open');
     });
 });
+
+/* web design */
+
+function changeMainImage(element) {
+    var imageUrl = element.getAttribute('src');
+    var modalImage = document.getElementById('modalImage');
+    var mainImage = document.getElementById('main-image').querySelector('img');
+
+    modalImage.src = imageUrl;
+    mainImage.src = imageUrl;
+    mainImage.setAttribute('data-bs-toggle', 'modal');
+    mainImage.setAttribute('data-bs-target', '#imageModal');
+    document.querySelector('.zoom-icon').setAttribute('data-bs-toggle', 'modal');
+    document.querySelector('.zoom-icon').setAttribute('data-bs-target', '#imageModal');
+}
+
+$(document).ready(function () {
+    $('.gallery-img, #main-image').click(function () {
+        var imageUrl = $(this).attr('src'); // Get the URL of the clicked image
+        var imageAlt = $(this).attr('alt'); // Get the alt text of the clicked image
+        $('#modalImage').attr('src', imageUrl); // Set the src attribute of the modal image
+        $('#imageModalLabel').text(imageAlt); // Set the text content of the modal title
+        $('#imageModal').modal('show'); // Show the modal
+    });
+});
+
