3974d302188e1ce3ffbe9230dd2b2fd91059ca83
designsbydarlene <agbayani.darlene@gmail.com>
2024-09-15 01:51:50 -0400

    add thumbnail

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,19 @@
 // loadNav.js
 fetch('nav.html')
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
     .then(data => {
         document.getElementById('nav-placeholder').innerHTML = data;
 
         // Now that the nav HTML is loaded, we can initialize the event listeners
         initializeNav();
+
+        // Find active link after the navigation is loaded
+        findActiveLink();
     })
     .catch(error => console.error('Error loading navigation:', error));
 
@@ -27,3 +35,30 @@ function initializeNav() {
         });
     }
 }
+
+// Function to find and highlight the active link
+function findActiveLink() {
+    const currentLocation = window.location.href;
+
+    // Select all links in the fullscreen menu
+    const links = document.querySelectorAll('.fullscreen-menu a');
+
+    links.forEach(link => {
+        // Get the href attribute of the link
+        const linkHref = link.getAttribute('href');
+
+        // Create an absolute URL for comparison
+        const absoluteLinkHref = linkHref.startsWith('http') ? linkHref : window.location.origin + '/' + linkHref;
+
+        // Check if current location matches the link href
+        if (absoluteLinkHref === currentLocation || 
+            (currentLocation === `${window.location.origin}/index.html` && linkHref === 'index.html')) {
+            link.classList.add('active'); // Add active class to current link
+        }
+    });
+
+    // Debugging logs
+    console.log('Current Location:', currentLocation);
+}
+
+// Make sure to call findActiveLink when the navigation is injected
