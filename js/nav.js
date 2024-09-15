2bea3a44d5c9d9464532a7c9dc074e81a9926d08
designsbydarlene <agbayani.darlene@gmail.com>
2024-09-14 22:47:00 -0400

    update logo color

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,29 @@
-/* Fullscreen nav */
-crossorigin="use-credentials"
-const hamburgerIcon = document.getElementById('hamburger-icon');
+// loadNav.js
+fetch('nav.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('nav-placeholder').innerHTML = data;
+
+        // Now that the nav HTML is loaded, we can initialize the event listeners
+        initializeNav();
+    })
+    .catch(error => console.error('Error loading navigation:', error));
+
+// Function to initialize event listeners for the navigation
+function initializeNav() {
+    const hamburgerIcon = document.getElementById('hamburger-icon');
     const fullscreenMenu = document.getElementById('fullscreen-menu');
 
-    hamburgerIcon.addEventListener('click', function() {
-        // Toggle classes for the hamburger icon and fullscreen menu
-        hamburgerIcon.classList.toggle('open');
-        fullscreenMenu.classList.toggle('open-menu');
+    if (hamburgerIcon && fullscreenMenu) {
+        hamburgerIcon.addEventListener('click', function() {
+            hamburgerIcon.classList.toggle('open');
+            fullscreenMenu.classList.toggle('open-menu');
 
-        // Add or remove the open-menu class to the body for styling changes
-        if (fullscreenMenu.classList.contains('open-menu')) {
-            document.body.classList.add('open-menu');
-        } else {
-            document.body.classList.remove('open-menu');
-        }
-    });
-    
+            if (fullscreenMenu.classList.contains('open-menu')) {
+                document.body.classList.add('open-menu');
+            } else {
+                document.body.classList.remove('open-menu');
+            }
+        });
+    }
+}
